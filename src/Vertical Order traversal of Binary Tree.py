3b058df70302e868fd5d9938e85a998c9523189b
crazykunal <42700979+crazykunal@users.noreply.github.com>
2021-02-14 00:50:33 -0800

    new tree problem solved

---
+++ src/Vertical Order traversal of Binary Tree.py
@@ -0,0 +1,19 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return a list of list of integers
+    def verticalOrderTraversal(self, root):
+        import collections
+        cols = collections.defaultdict(list)
+        queue = [(root, 0)]
+        for node, i in queue:
+            if node:
+                cols[i].append(node.val)
+                queue += (node.left, i - 1), (node.right, i + 1)
+        return [cols[i] for i in sorted(cols)]
