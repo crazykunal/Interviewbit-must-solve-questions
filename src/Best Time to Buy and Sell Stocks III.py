cad491e62a4b1cd5630e0df530778ae5d84f5657
crazykunal <42700979+crazykunal@users.noreply.github.com>
2021-02-14 01:00:17 -0800

    new dp problem solved

---
+++ src/Best Time to Buy and Sell Stocks III.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxProfit(self, prices):
+        one_buy = two_buy =   sys.maxsize
+        one_profit = two_profit = 0
+        for p in prices:
+            one_buy = min(one_buy,p)
+            one_profit = max(one_profit,p - one_buy)
+            two_buy = min(two_buy,p - one_profit)
+            two_profit = max(two_profit,p - two_buy)
+        return two_profit
